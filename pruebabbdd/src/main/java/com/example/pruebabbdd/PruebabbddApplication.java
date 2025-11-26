33ac8f643217a357c879ed3e76629cf52ca854f8
diego.capellan-fernandez@iesruizgijon.com <diego.capellan-fernadez@iesruizgijon.com>
2025-11-26 21:25:00 +0100

    proyecto de prueba creado

---
+++ pruebabbdd/src/main/java/com/example/pruebabbdd/PruebabbddApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebabbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebabbddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebabbddApplication.class, args);
+	}
+
+}
---
+++ pruebabbdd/src/test/java/com/example/pruebabbdd/PruebabbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebabbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebabbddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
