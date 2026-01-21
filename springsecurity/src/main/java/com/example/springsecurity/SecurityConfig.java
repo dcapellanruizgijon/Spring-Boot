b5291389775017f395ab50e9feccdfc75f1a74cc
dcapellanruizgijon <diego.capellan-fernandez@iesruizgijon.com>
2026-01-21 15:07:33 +0100

    Ejercicio sencillos de Security realizados

--- springsecurity/src/main/java/com/example/springsecurity/SecurityConfig.java
+++ springsecurity/src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -1,6 +1,43 @@
 package com.example.springsecurity;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+//aqui dentro se nos permite crear mas de un usuario
+@Configuration
 public class SecurityConfig {
+    //@Bean le dice a spring que esto es un OBJETO que queremos que gestione
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();// Es una interfaz que Spring Security usa para cargar información de usuarios (es lo que devolvemos al final, despues de añadir este usuario)
 
+        //creamos el usuario
+        UserDetails user1 = User.builder().username("admin")
+        .password(this.passwordEncoder().encode("admin"))
+        .authorities("write")//permisos
+        .build();//termina la construcción
+
+        //otro usuario
+        UserDetails user2 = User.builder().username("normal")
+        .password(this.passwordEncoder().encode("normal"))
+        .authorities("write")
+        .build();
+
+        //Añade los usuarios al gestor de usuarios
+        userDetailsService.createUser(user1);
+        userDetailsService.createUser(user2);
+
+        return userDetailsService;//devuelve el gestor con el usuario ya registrado
+    }
+    @Bean//crea otro objeto gestionado por spring
+    public PasswordEncoder passwordEncoder() {//passwordEncoder== Interfaz para encriptar/verificar contraseñas
+        return new BCryptPasswordEncoder();//implementación que usa un algoritmo para codificar
+    }
     
+
 }
--- springsecurity/src/main/java/com/example/springsecurity/controlador.java
+++ springsecurity/src/main/java/com/example/springsecurity/controlador.java
@@ -1,5 +1,21 @@
 package com.example.springsecurity;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//@Controller se usa para aplicaciones web tradicionales que devuelven vistas HTML, 
+//mientras que @RestController es una especialización para APIs RESTful que devuelven datos (JSON/XML) directamente
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller  
 public class controlador {
+    
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+
+    
     
 }
